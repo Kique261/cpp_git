5fc985eaadb9eca46cb9e0c656d11079de397182
Kique261 <92430275+Kique261@users.noreply.github.com>
2023-10-23 08:54:53 +0800

    更新 01Knap.cpp
    1023_0854

--- 01Knap.cpp
+++ 01Knap.cpp
@@ -1,40 +1,63 @@
 #include<iostream>
 #include<vector>
-#include<chrono>
 #include<string>
 #include<algorithm>
 #include<cstdlib>
 #include<iomanip>
-#include<windows.h>
 
 using namespace std;
-using namespace chrono;
 
 struct objects
 {
     int value = 0;
     int weight = 0;
+    int chosen = 0;
 };
 
 void random_obj(vector<objects>& arr,int n){
     srand(time(nullptr));
     for (int i = 0; i < n; i++)
     {
-        arr.push_back({rand()%49+1,rand()%19+1});
+        arr.push_back({rand()%9+1,rand()%9+1,0});
     }
     
 }
 
-string Backtrack(int t){
-    string x;
-    if(t>n){
-        return x;
-        x = "";
+bool overweight(int t,int c,vector<objects>&all){
+    int sum=0;
+    for(int i=0;i<=t;i++){
+        if(all[i].chosen==1){
+            sum+=all[i].weight;
+        }
+    }
+    if(sum>c){
+        return false;
+    }
+    return true;
+}
+
+void Backtrack(int t,int c,vector<objects>&all){
+    if(t>=all.size()){
+        return;
+    }
+    else for(int i=0;i<=1;i++){
+        all[i].chosen=i;
+        if(overweight(t,c,all)){
+            Backtrack(t+1,c,all);
+        }
+    }
+}
+
+void print_chosen(vector<objects>&all){
+    for(int i=0;i<all.size();i++){
+        cout<<all[i].weight<<all[i].chosen;
     }
 }
 
 int main(){
     vector<objects> all;
-    random_obj(all,3);
-
+    random_obj(all,7);
+    int c=100;
+    Backtrack(0,c,all);
+    print_chosen(all);
 }
