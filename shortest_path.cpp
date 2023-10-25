a12c2156bfd6a7631a3236f9ac7c69370e566451
Kique261 <2107342958@qq.com>
2023-10-25 12:07:56 +0800

    modified:   shortest_path.cpp 	modified:   shortest_path.exe 	modified:   tempCodeRunnerFile.cpp 	modified:   tempCodeRunnerFile.exe

--- shortest_path.cpp
+++ shortest_path.cpp
@@ -10,8 +10,6 @@
 
 using namespace std;
 
-const int MAX_PATH=50;
-
 class MinNode{
     public:
         int id;
@@ -25,16 +23,15 @@ class Graph{
     public:
         void Rand_Graph(vector<vector<int>>& edges, int n_node, int n_edge) {
             srand(time(NULL));
-            edges.assign(n_node, vector<int>(n_node, 0));
+            edges.assign(n_node, vector<int>(n_node, -1));
             for (int i = 0; i < n_edge; i++) {
                 int u = rand() % n_node;
                 int v = rand() % n_node;
-                while (u == v || (edges[u][v]>0&&edges[u][v]<100)) {
+                while (u == v || (edges[u][v]>0)) {
                     v = rand() % n_node;
                 }
-                int p1 = rand()%100;
-                edges[u][v] = p1;
-                edges[v][u] = p1;
+                edges[u][v] = rand()%max_path;
+                edges[v][u] = rand()%max_path;
             }
             // 遍历每个顶点，确保连通性
             vector<bool> visited(n_node, false);
@@ -46,7 +43,7 @@ class Graph{
                 visited[u] = true;
 
                 for (int v = 0; v < n_node; v++) {
-                    if ((edges[u][v]>0&&edges[u][v]<100)&& !visited[v]) {
+                    if ((edges[u][v]>0)&& !visited[v]) {
                         queue.push_back(v);
                     }
                 }
@@ -54,17 +51,17 @@ class Graph{
             for (int u = 0; u < n_node; u++) {
                 if (!visited[u]) {
                     int v = rand() % n_node;
-                    while (u == v || (edges[u][v]>0&&edges[u][v]<100)) {
+                    while (u == v || (edges[u][v]>0)) {
                         v = rand() % n_node;
                     }
-                    int p2 = rand()%100;
-                    edges[u][v] = p2;
-                    edges[v][u] = p2;
+                    int p2 = rand()%max_path;
+                    edges[u][v] = rand()%max_path;
+                    edges[v][u] = rand()%max_path;
                 }
             }
         }
 
-        void print_edges(const vector<vector<int>>& edges) {
+        void print_edges(vector<vector<int>>& edges) {
             int n = edges.size();
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
@@ -74,23 +71,90 @@ class Graph{
             }
         }
 
-        void Graph2(){
+        Graph(){
             cout << "请输入顶点数和边数(随机生成边，为了保证连通实际边数会>=输入边数)："<<endl;
             cin >> n_node >> n_edge;
-            vector<int> ie1;
-            edges.push_back(ie1);
-            for (int i = 1; i <= n_node; i++)
-            {
-                vector<int> ie2(n_node+1,1e9);
-                edges.push_back(ie2);
-            }
+            // vector<int> ie1;
+            // edges.push_back(ie1);
+            // for (int i = 1; i <= n_node; i++)
+            // {
+            //     vector<int> ie2(n_node+1,1e9);
+            //     edges.push_back(ie2);
+            // }
             Rand_Graph(edges,n_node,n_edge);
+            cout << "生成的邻接矩阵为："<<endl;
             print_edges(edges);
+            prev.assign(n_node+1,-1);
+            dist.assign(n_node,1e9);
+            cout << "请输入起始点："<<endl;
+            cin >> v >> u;
+            cout << '\n';
         }
 
+        void short_path(){
+            priority_queue<MinNode> Q;
+            MinNode E ={v,0};
+            dist[v]=0;
+            prev[v]=0;
+            while (1)
+            {
+                for (int i = 1; i <= n_node; i++)
+                {
+                    if(edges[E.id][i]>0){
+                        int temp = E.length + edges[E.id][i];
+                        if (temp<dist[i])
+                        {
+                            MinNode active = {i,temp};
+                            prev[i] = E.id;
+                            dist[i] = temp;
+                            Q.push(active);
+                        }
+                    }
+                }
+                if(!Q.empty()){
+                    E = Q.top();
+                    Q.pop();
+                }
+                else{
+                    break;
+                }
+            }
+            
+        }
 
+        void print_solution(){
+            int p=u;
+            vector<int> path;
+            while (prev[p]!=0)
+            {
+                path.push_back(p);
+                p=prev[p];
+            }
+            path.push_back(v);
+            reverse(path.begin(),path.end());
+            // cout << "dist:"<<endl;
+            // for (int i = 1; i < dist.size(); i++)
+            // {
+            //     cout << dist[i]<< " ";
+            // }
+            // cout << endl;
+            // cout << "prev:"<<endl;
+            // for (int i = 1; i < prev.size(); i++)
+            // {
+            //     cout << prev[i] <<" ";
+            // }
+            // cout << endl;
+            cout << v <<"->"<< u <<"的最短路径为："<<endl;
+            cout << v;
+            for (int i = 1; i < path.size(); i++)
+            {
+                cout << "->" <<path[i];
+            }
+            cout << endl;
+        }
 
     public:
+        int max_path = 50;//生成的边长上限
         int n_node;
         int n_edge;
         vector<int> prev;//前驱数组
@@ -100,8 +164,9 @@ class Graph{
         int u;
 };
 
-Graph G;
 
 int main(){
-    G.Graph2();
+    Graph G;
+    G.short_path();
+    G.print_solution();
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,82 +1,101 @@
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
 #include <vector>
+#include <random>
+#include <ctime>
+#include <algorithm>
 
-using namespace std;
+std::mt19937_64 mt(std::time(nullptr));
 
-// 随机生成连通图的函数
-void generateRandomGraph(vector<vector<int>>& edges, int n_node, int n_edge) {
-    srand(time(NULL));
+// 判断有向图是否连通
+bool is_connected(const std::vector<std::vector<int>>& edges, int n_node) {
+    std::vector<bool> visited(n_node, false);
+    std::vector<int> stack;
+    
+    stack.push_back(0); // 从节点0开始遍历
+    visited[0] = true;
 
-    // 初始化邻接矩阵为全0
-    edges.assign(n_node, vector<int>(n_node, 0));
+    while (!stack.empty()) {
+        int node = stack.back();
+        stack.pop_back();
 
-    // 随机生成边
-    for (int i = 0; i < n_edge; i++) {
-        int u = rand() % n_node; // 随机生成起始顶点
-        int v = rand() % n_node; // 随机生成目标顶点
-
-        // 目标顶点不能与起始顶点相同，也不能已经存在边
-        while (u == v || (edges[u][v]>0&&edges[u][v]<100)) {
-            v = rand() % n_node;
-        }
-        int p1 = rand()%100;
-        edges[u][v] = p1;
-        edges[v][u] = p1; // 无向图的边是双向的
-    }
-
-    // 遍历每个顶点，确保连通性
-    vector<bool> visited(n_node, false);
-    vector<int> queue;
-    queue.push_back(0); // 从顶点0开始进行广度优先搜索
-
-    while (!queue.empty()) {
-        int u = queue.back();
-        queue.pop_back();
-        visited[u] = true;
-
-        for (int v = 0; v < n_node; v++) {
-            if ((edges[u][v]>0&&edges[u][v]<100)&& !visited[v]) {
-                queue.push_back(v);
+        for (int i = 0; i < n_node; ++i) {
+            if (edges[node][i] != 0 && !visited[i]) {
+                visited[i] = true;
+                stack.push_back(i);
             }
         }
     }
 
-    // 如果有未访问的节点，添加一条边以确保连通性
-    for (int u = 0; u < n_node; u++) {
-        if (!visited[u]) {
-            int v = rand() % n_node;
-            while (u == v || (edges[u][v]>0&&edges[u][v]<100)) {
-                v = rand() % n_node;
-            }
-            int p2 = rand()%100;
-            edges[u][v] = p2;
-            edges[v][u] = p2;
-        }
-    }
+    // 检查是否所有节点都被访问到
+    return std::all_of(visited.begin(), visited.end(), [](bool v) { return v; });
 }
 
-// 打印邻接矩阵
-void printAdjacencyMatrix(const vector<vector<int>>& edges) {
-    int n = edges.size();
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cout << edges[i][j] << " ";
-        }
-        cout << endl;
+// 修复图的连通性
+void fix_connectivity(std::vector<std::vector<int>>& edges, int n_node) {
+    if (is_connected(edges, n_node)) {
+        std::cout << "图已经是连通的，无需修改。" << std::endl;
+        return;
     }
+
+    // 从随机节点开始，使用深度优先搜索（DFS）添加边，直到图连通为止
+    std::uniform_int_distribution<int> dist(0, n_node - 1);
+    int start_node = dist(mt);
+
+    std::vector<int> stack;
+    std::vector<bool> visited(n_node, false);
+    visited[start_node] = true;
+    stack.push_back(start_node);
+
+    while (!is_connected(edges, n_node)) {
+        if (stack.empty()) {
+            // 如果堆栈为空，选择一个未访问的节点作为新的起点
+            for (int i = 0; i < n_node; ++i) {
+                if (!visited[i]) {
+                    start_node = i;
+                    visited[i] = true;
+                    stack.push_back(start_node);
+                    break;
+                }
+            }
+        } else {
+            int current_node = stack.back();
+            stack.pop_back();
+
+            int target_node = dist(mt);
+            if (target_node != current_node && !visited[target_node]) && edges[current_node][target_node] == 0) {
+                edges[current_node][target_node] = 1;
+                visited[target_node] = true;
+                stack.push_back(target_node);
+            }
+        }
+    }
+
+    std::cout << "已修复为连通图。" << std::endl;
 }
 
 int main() {
-    int n_node = 6; // 顶点个数
-    int n_edge = 8; // 边个数
+    int n_node = 5; // 顶点个数
 
-    vector<vector<int>> edges;
-    generateRandomGraph(edges, n_node, n_edge);
+    // 初始化邻接矩阵（示例，你可以根据需要更改）
+    std::vector<std::vector<int>> edges = {
+        {0, 1, 1, 0, 0},
+        {0, 0, 0, 1, 0},
+        {0, 0, 0, 0, 1},
+        {0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0}
+    };
 
-    cout << "邻接矩阵：" << endl;
-    printAdjacencyMatrix(edges);
+    // 修复连通性
+    fix_connectivity(edges, n_node);
+
+    // 输出修复后的邻接矩阵
+    std::cout << "修复后的有向连通图邻接矩阵：" << std::endl;
+    for (int i = 0; i < n_node; ++i) {
+        for (int j = 0; j < n_node; ++j) {
+            std::cout << edges[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
 
     return 0;
 }
